8f5bb3d143f8b695feb1729b5bb4e8f268733836
rm86559 <sosofadoharry@gmail.om>
2023-05-18 12:26:12 -0300

    Criada classe pessoa

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         // Aula introdutória de Orientação á Objetos
  Pessoa adao = new Pessoa();
+ //Declaração de objeto
+        Pessoa qualquer;
+        //Instanciação de objeto
+        qualquer = new Pessoa();
+        //Definição do formato da pessoa qualquer
+        qualquer.nome="Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -2,4 +2,9 @@ public class Pessoa {
     //Atributos da pessoas
     public String nome;
     public String sobrenome;
+    //Métodos de classe
+    public void falar(){
+        System.out.println("falei");
+    }
+
 }
